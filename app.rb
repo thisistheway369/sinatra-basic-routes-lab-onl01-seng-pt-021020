69f32f518311e4183ec691fcbd58cb3b27def774
thisistheway369 <thisistheway369@icloud.com>
2020-05-14 14:04:31 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Shana"
+  end
+
+  get '/hometown' do
+    "My hometown is Petaluma"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Hotline Bling'"
+  end
 end
